a00b11e19a5c4be67dfc4a9185e4d4d2745fc8d7
Andriy <andygrigs88@gmail.com>
2024-07-15 07:17:21 +0200

    first task done

---
+++ task_6_01/total_salary.py
@@ -0,0 +1,29 @@
+def total_salary(path):
+    try:
+        with open(path, "r", encoding='utf-8') as file:
+            total_salary = 0
+            developer_count = 0
+
+            for line in file:
+                try:
+                    name, salary_str = line.strip().split(',')
+                    total_salary += int(salary_str)
+                    developer_count += 1
+                except ValueError:
+                    print(f"Помилка обробки рядка {line.strip()}")
+            
+            average_salary = total_salary / developer_count if developer_count else 0
+            
+            return total_salary, int(average_salary)
+    
+    except FileNotFoundError:
+        print("Файл не знайдено.")
+        return None, None
+    except Exception as e:
+        print(f"Ой, помилочка: {e}")
+        return None, None
+
+# Приклад використання функції:
+path_to_file = "salary_file.txt"
+total, average = total_salary(path_to_file)
+print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")
